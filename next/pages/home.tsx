7da1c0344c92686f4e8746a8f8110ce7725492e2
takahiro <takapi854@gmail.com>
2022-05-05 19:48:21 +0900

    http request(GET) by fetch

--- next/pages/home.tsx
+++ next/pages/home.tsx
@@ -8,6 +8,12 @@ import {Parent} from '../components/SampleComponent1'
 const Home: NextPage = () => {
   const [id, setId] =useState('test')
 
+  useEffect(() => {
+    fetch("http://localhost:8080/rest", {method: 'GET'})
+    .then((res)=> console.log(res))
+    
+  }, [])
+
   let i = 6666
 
   return (
