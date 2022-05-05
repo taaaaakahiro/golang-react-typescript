53f49445f4950b7395c6fd888084582c1c52514c
takahiro <takapi854@gmail.com>
2022-05-05 18:55:30 +0900

    update main.go

--- api/main.go
+++ api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// "time"
 	"html/template"
 )
 
@@ -20,21 +19,34 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 // REST API
 func restHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
+	// CASE1 ~switch文の場合~
+	switch r.Method {
+	case "GET":
 		fmt.Fprintln(w, "GET called!!")
-	} else if r.Method == "POST" {
+	case "POST":
 		fmt.Fprintln(w, "POST called!!")
-	} else if r.Method == "PUT" {
-		fmt.Fprintln(w, "PUT called!!")
-	} else if r.Method == "DELETE" {
+	case "PUT":
+		fmt.Fprintln(w, "UPDATE called!!")
+	case "DELETE":
 		fmt.Fprintln(w, "DELETE called!!")
+	default:
+		w.WriteHeader(405)
 	}
+	// CASE2 ~if文の場合~
+	// if r.Method == "GET" {
+	// 	fmt.Fprintln(w, "GET called!!")
+	// } else if r.Method == "POST" {
+	// 	fmt.Fprintln(w, "POST called!!")
+	// } else if r.Method == "PUT" {
+	// 	fmt.Fprintln(w, "PUT called!!")
+	// } else if r.Method == "DELETE" {
+	// 	fmt.Fprintln(w, "DELETE called!!")
+	// }
 }
 
 // Template Engine
 func templateHandler(w http.ResponseWriter, r *http.Request){
 	t, _ := template.ParseFiles("template/tmpl.html", "template/tmpl2.html")
-	// t.Execute(w, time.Now())
 	DaysOfWeek := []string{"日", "月", "火", "水", "木", "金", "土"}
 	t.Execute(w, DaysOfWeek)
 }
