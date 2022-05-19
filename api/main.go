6df607f3acc98e596a0c1892a112a6f7dd6ea628
takahiro <takapi854@gmail.com>
2022-05-19 22:32:12 +0900

    update main.go CORS

--- api/main.go
+++ api/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -17,6 +19,14 @@ func main() {
 
 	// chi
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }))
 	r.Use(middleware.Logger)
 	r.Get("/welcome", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
